622ad6f39427c5c43ff7340bf3f3fb34916d07ff
mohamed nasser <momom_2011@hotmail.com>
2025-06-10 22:18:32 +0300

    chore: remove next-intl dependency and related provider component

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -2,16 +2,13 @@ import React from 'react'
 
 import { HeaderThemeProvider } from './HeaderTheme'
 import { ThemeProvider } from './Theme'
-import { NextIntlClientProvider } from 'next-intl'
 
 export const Providers: React.FC<{
   children: React.ReactNode
 }> = ({ children }) => {
   return (
     <ThemeProvider>
-      <HeaderThemeProvider>
-        <NextIntlClientProvider>{children}</NextIntlClientProvider>
-      </HeaderThemeProvider>
+      <HeaderThemeProvider>{children}</HeaderThemeProvider>
     </ThemeProvider>
   )
 }
